35deee547811b24f96df46490c1ca94b89022bae
tticasso <tungtkhe171025@fpt.edu.vn>
2023-09-20 22:47:03 +0700

    update file React

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
